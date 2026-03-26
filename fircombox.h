92a60eabcbc61a8998aea6cccf28087b3709e042
wjd11235 <1916597576@qq.com>
2026-03-26 22:11:06 +0800

    second commit

---
+++ fircombox.h
@@ -0,0 +1,15 @@
+#ifndef FIRCOMBOX_H
+#define FIRCOMBOX_H
+
+#include <QWidget>
+
+class FirComBox : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit FirComBox(QWidget *parent = nullptr);
+
+signals:
+};
+
+#endif // FIRCOMBOX_H
